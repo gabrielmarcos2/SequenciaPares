370c4b4e0a0a994e0ecda18da682a3728d03696c
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-05-27 19:44:01 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write($"Números pares entre0 e ?: ");
+int i = Convert.ToInt32(Console.ReadLine()!);
+int j = 0;
+
+while (j <= i)
+{
+    Console.Write($"{j} ");
+    j+=2;
+}
+
+Console.WriteLine();
